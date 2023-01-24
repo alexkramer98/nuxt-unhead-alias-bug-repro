34cb25af8a59a9999a79de4e07650e3dc5049037
Alex Kramer <alexkramer98@gmail.com>
2023-01-24 13:24:50 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <span>
+        should be red
+    </span>
+</template>
+
+<script setup lang="ts">
+useHead({
+    link: [{
+        rel: 'stylesheet',
+        href: '~/assets/css/default.css',
+    }],
+})
+</script>
